6f8746007c5fa01f013a335fc1d035fd8b12023d
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2023-12-24 22:36:39 +0530

    Add reverseArray.cpp comment

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -1,10 +1,10 @@
 /**
  * @brief Reverses the elements of a given array.
- * 
+ *
  * This function takes an array as input and reverses its elements in-place.
  * It uses a two-pointer approach to swap the elements from the start and end of the array.
  * The reversed array is then returned.
- * 
+ *
  * @param arr The input array to be reversed.
  * @return vector<int> The reversed array.
  */
@@ -12,7 +12,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> reverseArray(vector<int>& arr)
+vector<int> reverseArray(vector<int> &arr)
 {
     int s = 0, e = arr.size() - 1;
 
@@ -30,6 +30,8 @@ int main()
 {
     vector<int> arr = {1, 2, 3, 4, 5};
     vector<int> res = reverseArray(arr);
+
+    // Printing the reversed array
     for (int i = 0; i < res.size(); i++)
     {
         cout << res[i] << " ";

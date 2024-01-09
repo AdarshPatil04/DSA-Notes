f40e9a8c9503d2707d5c3e4849ac3c66976f7819
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2024-01-09 23:40:58 +0530

    Add bubbleSort.cpp

--- Arrays/Recursion/bubbleSort.cpp
+++ Arrays/Recursion/bubbleSort.cpp
@@ -1,26 +1,30 @@
 /**
  * @brief This program implements the bubble sort algorithm using recursion.
  *        It sorts an array of integers in ascending order.
- * 
+ *
  * @param arr The array to be sorted.
  * @param n The size of the array.
  */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[], int n){
+void bubbleSort(int arr[], int n)
+{
 
     // base case
-    // if n becomes 1 then return
-    if(n == 1){
+    // if n becomes 1 or 0 then return
+    if (n == 0 || n == 1)
+    {
         return;
     }
 
     // recursive case
     // bubble the largest element to the end
-    for(int i = 0; i < n - 1; i++){
-        if(arr[i] > arr[i + 1]){
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
             swap(arr[i], arr[i + 1]);
         }
     }
@@ -29,13 +33,15 @@ void bubbleSort(int arr[], int n){
     bubbleSort(arr, n - 1);
 }
 
-int main(){
+int main()
+{
     int arr[] = {5, 4, 3, 2, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     // Printing the sorted array
     bubbleSort(arr, n);
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     return 0;

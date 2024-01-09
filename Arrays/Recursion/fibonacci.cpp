598f7c70657016ca60b49021f44da3eb5c6e43ef
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2024-01-09 20:43:04 +0530

    Add fibonacci.cpp

---
+++ Arrays/Recursion/fibonacci.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file fibonacci.cpp
+ * @brief This file contains the implementation of the Fibonacci series using recursion.
+ * The Fibonacci series is a sequence of numbers in which each number is the sum of the two preceding ones.
+ * This program calculates the nth Fibonacci number using recursion.
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief Calculates the nth Fibonacci number using recursion.
+ * @param n The position of the Fibonacci number to be calculated.
+ * @return The nth Fibonacci number.
+ */
+
+int fibonacci(int n){
+
+    // base case
+    if(n == 0 || n == 1){
+        return n;
+    }
+
+    // recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(){
+    int n = 5;
+
+    // prints the nth Fibonacci number
+    cout<<fibonacci(n);
+    return 0;
+}

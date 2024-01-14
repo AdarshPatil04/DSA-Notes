7df7a6847bdd0aae9099dae720e92d61196093f3
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2024-01-14 23:03:17 +0530

    Add reverseDoublyLL.cpp

--- Linked List/reverseDoublyLL.cpp
+++ Linked List/reverseDoublyLL.cpp
@@ -1,3 +1,18 @@
+/**
+ * @brief This program demonstrates the reversal of a doubly linked list.
+ * 
+ * The program defines a Node class for a doubly linked list and provides functions to:
+ * - Reverse a doubly linked list
+ * - Print a doubly linked list
+ * - Insert a node at the end of a doubly linked list
+ * 
+ * The main function creates a doubly linked list with values 1, 2, 3, 4, 5, and performs the following steps:
+ * 1. Prints the original doubly linked list
+ * 2. Reverses the doubly linked list
+ * 3. Prints the reversed doubly linked list
+ * 
+ * @return 0 indicating successful execution of the program
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -45,18 +60,20 @@ void printDoublyLinkedList(Node* head) {
 // Function to insert a node at the end of a doubly linked list
 Node* insertAtEnd(Node* head, int value) {
     Node* newNode = new Node(value);
+    // If the doubly linked list is empty, then make the new node as head
     if (head == nullptr) {
         return newNode;
     }
-
+    // Otherwise, traverse the doubly linked list to go to the last node
     Node* temp = head;
     while (temp->next != nullptr) {
         temp = temp->next;
     }
-
+    // Change the next of the last node
     temp->next = newNode;
+    // Make the last node as the previous of the new node
     newNode->prev = temp;
-
+    // Return the head pointer of the doubly linked list
     return head;
 }
 

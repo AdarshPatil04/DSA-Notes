e6e59801e64a407b34f7e7b1b28029aa4f0247a3
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2024-01-28 21:56:05 +0530

    topView.cpp

--- Binary Tree/topView.cpp
+++ Binary Tree/topView.cpp
@@ -26,63 +26,65 @@ public:
     }
 };
 
-// Function to return a list of nodes visible from the top view 
+// Function to return a list of nodes visible from the top view
 // from left to right in Binary Tree.
 vector<int> topView(Node *root)
 {
     // ans: a vector to hold the final top view
     vector<int> ans;
-    
+
     // If root is NULL, return empty vector
-    if(root == NULL) 
+    if (root == NULL)
     {
         return ans;
     }
-    
+
     // topNode: a map to hold horizontal distance and node data
-    map<int,int> topNode;
-    
+    map<int, int> topNode;
+
     // q: a queue to hold node and its horizontal distance
-    queue<pair<Node*, int> > q;
-    
+    queue<pair<Node *, int>> q;
+
     // Push root to queue with horizontal distance as 0
     q.push(make_pair(root, 0));
-    
+
     // While there are nodes in queue
-    while(!q.empty()) {
+    while (!q.empty())
+    {
         // Pop a node from queue
-        pair<Node*, int> temp = q.front();
+        pair<Node *, int> temp = q.front();
         q.pop();
-        
+
         // Get the node and horizontal distance
-        Node* frontNode = temp.first;
+        Node *frontNode = temp.first;
         int hd = temp.second;
-        
+
         // If no value is present for a horizontal distance in map, add node data
-        if(topNode.find(hd) == topNode.end()) // topNode.end() return a fixed iterator "past the end"
-            topNode[hd] = frontNode -> data;
-            
+        if (topNode.find(hd) == topNode.end()) // topNode.end() return a fixed iterator "past the end"
+            topNode[hd] = frontNode->data;
+
         // If left child exists, push it to queue with horizontal distance decreased by 1
-        if(frontNode->left)
-            q.push(make_pair(frontNode->left, hd-1));
-        
+        if (frontNode->left)
+            q.push(make_pair(frontNode->left, hd - 1));
+
         // If right child exists, push it to queue with horizontal distance increased by 1
-        if(frontNode->right)
-            q.push(make_pair(frontNode->right, hd+1));
+        if (frontNode->right)
+            q.push(make_pair(frontNode->right, hd + 1));
     }
-    
+
     // Traverse the map topNode
-    for(auto i:topNode) 
+    for (auto i : topNode)
     {
         // Push node data to ans vector
         ans.push_back(i.second);
     }
-    
+
     // Return the final top view
     return ans;
 }
 
-int main(){
+int main()
+{
     // Create a binary tree
     Node *root = new Node(1);
     root->left = new Node(2);
@@ -91,10 +93,11 @@ int main(){
     root->left->right = new Node(4);
     root->left->right->right = new Node(5);
     root->left->right->right->right = new Node(6);
-    cout<<"Top view of the binary tree: ";
+    cout << "Top view of the binary tree: ";
     vector<int> ans = topView(root);
-    for(auto i:ans) {
-        cout<<i<<" ";
+    for (auto i : ans)
+    {
+        cout << i << " ";
     }
     return 0;
 }
